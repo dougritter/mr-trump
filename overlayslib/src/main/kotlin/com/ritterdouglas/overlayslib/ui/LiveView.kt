7c69b5a3916e5ed66b86dc5bc727d9aa998b4948
dougritter <drmachado@gmail.com>
2017-02-04 16:02:33 +0000

    created project with app and lib

---
+++ overlayslib/src/main/kotlin/com/ritterdouglas/overlayslib/ui/LiveView.kt
@@ -0,0 +1,104 @@
+package com.ritterdouglas.overlayslib.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.text.TextPaint
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+
+import com.ritterdouglas.overlayslib.R
+
+/**
+ * TODO: document your custom view class.
+ */
+class LiveView : FrameLayout {
+
+    val TAG: String = LiveView::class.simpleName!!
+
+    private var mExampleString: String = "LiveView"
+    private var mExampleColor = Color.RED
+    private var mExampleDimension = 0f
+
+    var exampleDrawable: Drawable? = null
+
+    private var mTextPaint: TextPaint? = null
+    private var mTextWidth: Float = 0.toFloat()
+    private var mTextHeight: Float = 0.toFloat()
+
+    var cameraPreview: FrameLayout? = null
+
+    constructor(context: Context) : super(context) {
+        init(context, null, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context, attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        init(context, attrs, defStyle)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet?, defStyle: Int) {
+        // Load attributes
+
+        View.inflate(context, R.layout.live_view, this)
+
+        cameraPreview = findViewById(R.id.cameraPreview) as FrameLayout?
+
+        val attributes = context.obtainStyledAttributes(
+                attrs, R.styleable.LiveView, defStyle, 0)
+
+    }
+
+    private fun invalidateTextPaintAndMeasurements() {
+        mTextPaint!!.textSize = mExampleDimension
+        mTextPaint!!.color = mExampleColor
+        mTextWidth = mTextPaint!!.measureText(mExampleString)
+
+        val fontMetrics = mTextPaint!!.fontMetrics
+        mTextHeight = fontMetrics.bottom
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        val paddingLeft = paddingLeft
+        val paddingTop = paddingTop
+        val paddingRight = paddingRight
+        val paddingBottom = paddingBottom
+
+        val contentWidth = width - paddingLeft - paddingRight
+        val contentHeight = height - paddingTop - paddingBottom
+
+        /*// Draw the text.
+        canvas.drawText(mExampleString!!,
+                paddingLeft + (contentWidth - mTextWidth) / 2,
+                paddingTop + (contentHeight + mTextHeight) / 2,
+                mTextPaint!!)
+
+        // Draw the example drawable on top of the text.
+        if (exampleDrawable != null) {
+            exampleDrawable!!.setBounds(paddingLeft, paddingTop,
+                    paddingLeft + contentWidth, paddingTop + contentHeight)
+            exampleDrawable!!.draw(canvas)
+        }*/
+    }
+
+    var exampleColor: Int
+        get() = mExampleColor
+        set(exampleColor) {
+            mExampleColor = exampleColor
+            invalidateTextPaintAndMeasurements()
+        }
+
+    var exampleDimension: Float
+        get() = mExampleDimension
+        set(exampleDimension) {
+            mExampleDimension = exampleDimension
+            invalidateTextPaintAndMeasurements()
+        }
+}
